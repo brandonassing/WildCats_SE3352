cee5c3e1cf6e4e1b1083610b3de5ca44d9130586
brandonassing <bassing@uwo.ca>
2016-12-02 16:37:07 -0500

    fixed database.js syntax

--- BackEnd/courseBlog/models/courseBlogDB.js
+++ BackEnd/courseBlog/models/courseBlogDB.js
@@ -1,23 +1,18 @@
 var mongoose = require('mongoose');
-var recordSchema = mongoose.Schema(
-    {
-        studentNo: string,
-        firstName: string,
-        lastName: string,
-        birthDate: string,
-        residency: string,
-        gender: string
-    }
-);
+var recordSchema = mongoose.Schema({
+    studentNo: String,
+    firstName: String,
+    lastName: String,
+    birthDate: String,
+    residency: String,
+    gender: String
+});
 
 var Records = mongoose.model('record', recordSchema);
 
 mongoose.connect('mongodb://localhost/courseBlog');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
     exports.Records = Records;
 });
-
-
-
