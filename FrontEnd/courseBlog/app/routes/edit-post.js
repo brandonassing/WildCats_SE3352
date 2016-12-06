65f4a616ee3a359d9cb68a21b09fd4d2e82b9977
brandonassing <bassing@uwo.ca>
2016-12-06 13:47:47 -0500

    working on edit-post.js

--- FrontEnd/courseBlog/app/routes/edit-post.js
+++ FrontEnd/courseBlog/app/routes/edit-post.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  studentNum: "",
+  fname: "",
+  lname: "",
+  birth: "",
+  residency: 0,
+  gender: 0,
+
+  store: Ember.inject.service(),
+
+  actions: {
+
+
+
+    ///////////////////////////////////////////////////////////
+    addNewPost() {
+      this.set('title', null);
+      this.set('body', null);
+      this.set('isAdding', true);
+    },
+    savePost() {
+      var myStore = this.get('store');
+      var newPost = myStore.createRecord('post', {
+        title: this.get('title'),
+        body: this.get('body')
+      });
+      newPost.save();
+      this.set('isAdding', false);
+    },
+
+    cancelPost() {
+      this.set('isAdding', false);
+    }
+    ///////////////////////////////////////////////////////////
+  }
+
+
+
+
 });
