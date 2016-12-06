3e0176bb9da5ab6b05dcdedbb09f2653d7aec24a
brandonassing <bassing@uwo.ca>
2016-12-06 18:46:21 -0500

    commit

--- FrontEnd/courseBlog/app/routes/contact.js
+++ FrontEnd/courseBlog/app/routes/contact.js
@@ -1,1 +1,20 @@
+import Ember from 'ember';
 
+export default Ember.Controller.extend({
+
+  sNumber: null,
+  actions: {
+    findStudent() {
+      var sNumber = this.get('sNumber');
+      window.alert(sNumber);
+      window.location = "http://localhost:4200/posts/" + sNumber;
+    }
+
+
+
+
+  }
+
+
+
+});
