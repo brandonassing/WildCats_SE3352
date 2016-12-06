1d90057800bb220f1aa025bd8968940dca4e519c
Kristian Strong <kstrong5@uwo.ca>
2016-12-06 15:41:21 -0500

    working on find record

---
+++ FrontEnd/courseBlog/app/controllers/contact.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  recordFound: false,
+  recordNotFound:false,
+  actions: {
+    /*
+     findStudent(){
+     if(this.store.findAll()){
+     this.set('recordFound', true);
+     }
+     else{
+     this.set('recordNotFound',true);
+     }
+
+     var myStore = this.get('store');
+     myStore.findAll();
+
+
+     }}*/
+    //
+
+  }
+});
+
