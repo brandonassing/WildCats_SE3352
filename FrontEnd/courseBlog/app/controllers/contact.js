4e7b5187ada1566d3d6abc8ed3fca4e6ce28bbd3
brandonassing <bassing@uwo.ca>
2016-12-06 19:29:04 -0500

    removed alert

--- FrontEnd/courseBlog/app/controllers/contact.js
+++ FrontEnd/courseBlog/app/controllers/contact.js
@@ -6,7 +6,6 @@ export default Ember.Controller.extend({
   actions: {
     findStudent(){
       var sNumber = this.get('sNumber');
-      window.alert(sNumber);
       window.location = "http://localhost:4200/posts/" + sNumber;
     }
 
